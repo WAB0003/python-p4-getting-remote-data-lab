9f5b6e3710e7fdbbb827f4ac4782df4151e5bd05
Bill Brown <bill.a.brown90@gmail.com>
2023-04-27 16:08:03 -0700

    jsonify remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -13,3 +13,7 @@ class GetRequester:
     def load_json(self):
         info_in_json = json.loads(self.get_response_body())
         return info_in_json
+
+testing = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(testing.get_response_body()) #Broken up and doesnt look good
+print(testing.load_json()) #Turns to an actual list of dictionaries
