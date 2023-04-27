5352ca2397b21b2b80b1655d61ef49fbf96d63d9
Bill Brown <bill.a.brown90@gmail.com>
2023-04-27 16:02:17 -0700

    getting json data from API

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        resp_body = requests.get(self.url)
+        return resp_body.content
 
     def load_json(self):
-        pass
+        info_in_json = json.loads(self.get_response_body())
+        return info_in_json
